ab2220ea01c42856d0db1b18296ae0d0ce270186
Facu-DM <149309942+Facu-DM@users.noreply.github.com>
2023-12-04 21:09:27 -0300

    Update Contador.js

--- src/Componentes/Contador.js
+++ src/Componentes/Contador.js
@@ -1,19 +1,52 @@
 import React from "react"
 import "./Contador.css"
 class Contador extends React.Component {
-    state = {
-        Contador : 0
+
+    constructor(props) {
+        super(props)
+        console.log('constructor Contador')
+
+        this.state = {
+            //Contador : 0
+            Contador: props.ini
+        }
+
+        /* this.Incrementar = this.Incrementar.bind(this) */
+
     }
+
+    /*state = {
+        //Contador : 0
+        Contador: this.props.ini
+    }*/
+
+    Incrementar() {
+        /* Incrementar = () => { */
+        const {id} = this.props
+        console.log('Incrementar: ' + id)
+        
+        this.setState(prevstate => ({Contador: prevstate.Contador + 1}))
+    }
+
     render () {
         const {id, color} = this.props
         const {Contador} = this.state
+
+        console.log('render contador', id)
+        
         return (
             <div className="Contador">
                 <div className="jumbotron" style={{background: color}}>
                     <h3>Contador Nro {id} </h3>
                     <hr/>
                     <h4 className="alert alert-info">Valor: {Contador}</h4>
-                    <button className="btn btn-success">Incrementar</button>
+
+
+                    <button className="btn btn-success" onClick={
+                        () => this.Incrementar()
+                        /* this.Incrementar */
+                        /* (function() {this.Incrementar()}).bind(this) */
+                    }>Incrementar</button>
 
                 </div>
             </div>
